112e3db8b68ae7876b7e373da76b50169c0074c6
U-IAI\mrenodin <mrenodin@MRENODIN.iai.pri>
2019-02-07 16:15:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ACE-TAO-mingw-installer
---
+++ config.h
@@ -0,0 +1,7 @@
+//#define ACE_LACKS_SYS_TIME_H 
+//#define ACE_LACKS_TIME_H
+//#define ACE_HAS_POSIX_TIME
+#define ACE_LACKS_CLOCKID_T
+#define HAVE_STRUCT_TIMESPEC
+#include "ace/config-win32.h"
+
